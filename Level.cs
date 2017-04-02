1d22764fe5eaafaaeeb9c25ca6099aff45509538
randombananana <jesse.schoonveld@gmail.com>
2017-04-03 01:22:50 +0200

    Add files via upload

---
+++ Level.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Level
+    {
+        private readonly Invader[] _invaders;
+
+        public Tower[] Towers { get; set; }
+
+        public Level(Invader[] invaders)
+        {
+            _invaders = invaders;
+        }
+
+        public bool Play() //Returns: true if the player wins, false otherwise.
+        {
+            // Run until all the invaders are neutralized or an invader reaches the end of the path.
+            int remainingInvaders = _invaders.Length;
+            while(remainingInvaders > 0)
+            {
+            //Each tower has opportunity to fire on invaders.
+            foreach(Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
+            
+            //Count and move the invaders that are still active.
+            remainingInvaders = 0;
+            foreach(Invader invader in _invaders)
+            {
+                if (invader.IsActive)
+                {
+                    invader.Move();
+
+                    if (invader.HasScored)
+                    {
+                        return false;
+                    }
+
+                    remainingInvaders++;
+                }
+            }
+            }
+            return true;
+        }
+        
+    }
+}
---
+++ Tower.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Tower
+    {
+        private const int _range = 1;
+        private const int _power = 1;
+        private const double _accuracy = .75;
+
+        private static readonly Random _random = new Random();
+
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location) 
+            {
+               _location = location;
+            }
+     
+        public bool IsSuccesfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {                   
+            foreach(Invader invader in invaders)
+            {
+                if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                {
+                    if (IsSuccesfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader!");
+                        if (invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader");
+                    }
+                    break;
+                }
+            }
+
+        }
+
+
+
+    }
+}
